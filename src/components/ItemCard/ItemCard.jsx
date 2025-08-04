cf2612b46f01abb4a2b5e4e54de496f888255bec
Bianca Nechita <nechita,bianca@yahoo.com>
2025-08-04 13:26:55 -0600

    updated importcard import

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -3,25 +3,28 @@ import defaultImage from '../../images/defaultimage.jpg';
 import dislikeHeart from '../../images/like.svg';
 import likeHeart from '../../images/liked.svg';
 import CurrentUserContext from '../../contexts/CurrentUserContext';
+import { useContext } from 'react';
 
-import { useContext } from 'react'
 export function shuffleItems(items = []) {
-  const shuffled = [...items]
+  const shuffled = [...items];
   for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
-  return shuffled
+  return shuffled;
 }
 
 function ItemCard({ item, onCardClick, onCardLike }) {
-  const currentUser = useContext(CurrentUserContext)
-  const isLiked = item.likes.some((id) => id === currentUser?._id)
+  const currentUser = useContext(CurrentUserContext);
+  const isLiked = item.likes.some((id) => id === currentUser?._id);
 
   const handlePreviewModal = () => {
-    onCardClick(item)
-  }
-  const handleLike = () => onCardLike({ id: item._id, isLiked })
+    onCardClick(item);
+  };
+
+  const handleLike = () => {
+    onCardLike({ id: item._id, isLiked });
+  };
 
   return (
     <li className="card">
@@ -32,7 +35,7 @@ function ItemCard({ item, onCardClick, onCardLike }) {
         src={typeof item.imageUrl === 'string' ? item.imageUrl : defaultImage}
         alt={item.name}
         onError={(e) => {
-          e.target.src = defaultImage
+          e.target.src = defaultImage;
         }}
       />
       {currentUser && (
@@ -45,7 +48,7 @@ function ItemCard({ item, onCardClick, onCardLike }) {
         </button>
       )}
     </li>
-  )
+  );
 }
 
-export default ItemCard
+export default ItemCard;
